a3ded0aef6c7027a6574f0fb6aab0f385482306e
avengang <544160071@qq.com>
2018-08-29 14:06:26 +0800

    代码更新

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 /* eslint-disable */
 require('./aop.js')
 require('./util.js')
+var _name_uuid_Map = {}
 var _viewDatas = {}
+var _vms = []
+// 动态加载页面文件的时候，页面文件还没加载等待，对应的baseview实例还没存在，此时要修改对象的值就先放到临时对象里面
+var wait2Update = {}
 function hash2name(hash) {
 	return encodeURIComponent(hash).replace(/%/g, '_')
 }
@@ -9,43 +13,49 @@ function isUndefinedOrNull(arg) {
 	if(arg === undefined || arg === null) return true
 	return false
 }
-function updateViewData(hash, key, value) {
-	hash = hash.trim()
-	if(hash[0] !== '#') hash = '#' + hash
-	hash = hash2name(hash)
-	if(_viewDatas[hash]) {
-		_viewDatas[hash][key] = value
+function updateViewData(name, key, value) {
+	var uuid = _name_uuid_Map[name]
+	if(!uuid) {
+		wait2Update[name] = {}
+		wait2Update[name][key] = value
 	} else {
-		for(var k in _viewDatas) {
-			if(k.split('_3F')[0] === hash) {
-				_viewDatas[k][key] = value
-			}
-		}
+		_viewDatas[uuid][key] = value
 	}
 }
 function updateCommonDataHelper(vm, key, value) {
+	if(!vm.common_data) {
+		return //其他未从baseview继承的页面
+	}
 	if(Object.prototype.toString.call(value) === '[object Array]') {
+		if(vm.common_data[key] === value) return
 		for(var j=0;j<value.length;j++) {
-			vm.$set(vm.common_data[key], j, window.deepCopy(value[j]))
+			vm.$set(vm.common_data[key], j, window.$deepCopy(value[j]))
 		}
 	} else if(Object.prototype.toString.call(value) === '[object object]') {
+		if(vm.common_data[key] === value) return
 		for(var _key in value) {
 			vm.$set(vm.common_data[key], _key, value[_key])
 		}
 	} else {
 		var obj = {}
 		obj[key] = value
-		vm.$set(vm.common_data, key, window.deepCopy(value))
-		// vm.common_data = Object.assign({}, vm.common_data, obj)
+		vm.$set(vm.common_data, key, window.$deepCopy(value))
 	}
 }
 function updateCommonData(key, value) {
 	if(!_viewDatas.common_data) _viewDatas.common_data = {}
 	_viewDatas.common_data[key] = value
-	updateCommonDataHelper(this, key, value)
+	for(var i=0,ii=_vms.length;i<ii;i++) {
+		var vm = _vms[i]
+		updateCommonDataHelper(vm, key, value)
+	}
 }
 var Baseview = function(config) {
-	if(!config.data()) {
+	var uuid = $getUuid()
+	var viewname = config.viewname || uuid
+	if(_name_uuid_Map[config.viewname]) throw new Error('viewname不能重复, 已经存在viewname = ' + config.viewname + ' 的对象')
+	_name_uuid_Map[viewname] = uuid
+	if(!config.data || !config.data()) {
 		config.data = function() {
 			return {
 				common_data: {
@@ -62,6 +72,7 @@ var Baseview = function(config) {
 			return d
 		}
 	}
+	_viewDatas[uuid] = config.data()
 	var callbacks = ['beforeCreate', 'created', 'beforeMount', 'mounted', 'beforeUpdate', 'updated', 'beforeDestroy', 'destroyed']
 	for(var k in config) {
 		if(config.hasOwnProperty(k) && !isUndefinedOrNull(config[k])) {
@@ -75,27 +86,52 @@ var Baseview = function(config) {
 		var _this = this;
 		(function(i) {
 			_this[callbacks[i]] = _this[callbacks[i]]._before_(function() {
-				if(i === 2) {
-					this._viewname = _this.name = hash2name(window.location.hash)
-					this.$set(this.common_data, '$init', 'init')
+				if(i === 0) { // beforeCreate
+					_vms.push(this)
+				}
+				if(i === 2) { // beforeMount
+					this._viewname = _this.name = uuid
 					for(var k in _viewDatas[_this.name]) {
 						if(this[k] === undefined) this.$set(this.$data, k, null)
 					}
 				}
+				if(i === 3) { // mounted
+					this.$el.classList.add('_js_isbaseview' + uuid) //给每个继承自该对象的页面根节点添加样式
+				}
 				_this['_' + callbacks[i]] && _this['_' + callbacks[i]]()
 			})._after_(function() {
-				if(i === 3) {
+				if(i === 3) { // mounted
+					var els = document.getElementsByClassName('_js_isbaseview' + uuid)
+					if(!els) return //没显示就不用浪费性能去更新data
 					for(var k in _viewDatas[_this.name]) {
+						if(this[k] === _viewDatas[_this.name][k]) continue // 没变化，不更新
 						if(Object.prototype.toString.call(_viewDatas[_this.name][k]) === '[object Array]') {
 							for(var j=0;j<_viewDatas[_this.name][k].length;j++) {
-								this.$set(this[k], j, window.deepCopy(_viewDatas[_this.name][k][j]))
+								this.$set(this[k], j, window.$deepCopy(_viewDatas[_this.name][k][j]))
 							}
 						} else if(Object.prototype.toString.call(_viewDatas[_this.name][k]) === '[object object]') {
 							for(var _key in _viewDatas[_this.name][k]) {
 								this.$set(this[k], _key, _viewDatas[_this.name][k][_key])
 							}
 						} else {
-							this.$set(this[k], k, _viewDatas[_this.name][k])
+							this[k] = _viewDatas[_this.name][k]
+						}
+					}
+					for(var commonk in _viewDatas.common_data) {
+						updateCommonData(this, commonk, _viewDatas.common_data[commonk])
+					}
+				}
+				if(i === 6) { // beforeDestroy
+					for(var i=0,ii=_vms.length;i<ii;i++) {
+						if(_vms[i] === this) {
+							_vms.splice(i, 1)
+							return
+						}
+					}
+					for(var i=0,ii=_viewDatas.length;i<ii;i++) {
+						if(_viewDatas[i] === this) {
+							_viewDatas.splice(i, 1)
+							return;
 						}
 					}
 				}
@@ -106,24 +142,17 @@ var Baseview = function(config) {
 	var _this = this;
 	if(this.beforeRouteLeave) {
 		this.beforeRouteLeave = this.beforeRouteLeave._before_(function() {
-			_viewDatas[_this.name] = window.deepCopy(this._data)
+			_viewDatas[_this.name] = window.$deepCopy(this._data)
 		})
 	} else {
 		this.beforeRouteLeave = function(to, from, next) {
-			_viewDatas[_this.name] = window.deepCopy(this._data)
+			_viewDatas[_this.name] = window.$deepCopy(this._data)
 			next();
 		}
 	}
 }
+window.Baseview = Baseview
 function install(Vue, options) {
-	Vue.mixin({
-		mounted: function () {
-			for(var commonk in _viewDatas.common_data) {
-				updateCommonDataHelper(this, commonk, _viewDatas.common_data[commonk])
-			}
-		}
-	})
-	window.Baseview = Baseview
 	Vue.prototype.$updateView = updateViewData
 	Vue.prototype.$updateCommon = updateCommonData
 }
