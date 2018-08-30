2c81b1d83f901267b3a80c1920f943670ac92304
avengang <544160071@qq.com>
2018-08-30 14:46:14 +0800

    update code

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-require('./aop.js')
 import util from './util.js'
 console.log(util)
 var _name_uuid_Map = {}
@@ -70,7 +69,7 @@ function updateViewData() {
 }
 function updateCommonDataHelper(vm, key, value) {
 	if(!vm.common) {
-		return //其他未从baseview继承的页面
+		return //其他未从VueData继承的页面
 	}
 	if(Object.prototype.toString.call(value) === '[object Array]') {
 		if(vm.common[key] === value) return
@@ -128,99 +127,92 @@ var VueData = function(config) {
 			config.props.viewtag = String
 		}
 	}
+	var oldBeforeMount = config.beforeCreate
+	config.beforeCreate = function() {
+		_vms.push(this)
+		oldBeforeMount && oldBeforeMount.bind(this)()
+	}
+	var oldBeforeMount = config.beforeMount
+	config.beforeMount = function() {
+		this.configviewname = viewname
+		this._viewname = uuid
+		this.cache = cache
+		this.$set(this.common, "randNum", util.$getUuid())
+		var viewtag = this._props.viewtag || 'default'
+		if(this.cache) { // 如果需要缓存的话就要把该对象data加入字段
+			for(var k in _viewDatas[this._viewname][viewtag]) {
+				if(this[k] === undefined) this.$set(this.$data, k, null)
+			}
+		}
+		oldBeforeMount && oldBeforeMount.bind(this)()
+		console.log(this)
+	}
+	var oldMounted = config.mounted
+	config.mounted = function() {
+		oldMounted && oldMounted.bind(this)()
+		var viewtag = this._props.viewtag || 'default'
+		if(this.cache) { // 有指定该对象需要缓存的话就要在渲染完之后加入缓存内容
+			var viewDatas = _viewDatas[this._viewname][viewtag]
+			for(var k in viewDatas) {
+				if(this[k] === viewDatas[k]) continue // 没变化，不更新
+				if(Object.prototype.toString.call(viewDatas[k]) === '[object Array]') {
+					for(var j=0;j<viewDatas[k].length;j++) {
+						this.$set(this[k], j, util.$deepCopy(viewDatas[k][j]))
+					}
+				} else if(Object.prototype.toString.call(viewDatas[k]) === '[object object]') {
+					for(var _key in viewDatas[k]) {
+						this.$set(this[k], _key, viewDatas[k][_key])
+					}
+				} else {
+					this[k] = viewDatas[k]
+				}
+			}
+			_viewDatas[this._viewname][viewtag] = null
+		}
+		if(wait2Update[viewname] && wait2Update[viewname][viewtag]) {
+			var waitData = wait2Update[viewname][viewtag]
+			for(var k in waitData) {
+				if(this[k] === waitData[k]) continue // 没变化，不更新
+				if(Object.prototype.toString.call(waitData[k]) === '[object Array]') {
+					for(var j=0;j<waitData[k].length;j++) {
+						this.$set(this[k], j, util.$deepCopy(waitData[k][j]))
+					}
+				} else if(Object.prototype.toString.call(waitData[k]) === '[object object]') {
+					for(var _key in waitData[k]) {
+						this.$set(this[k], _key, waitData[k][_key])
+					}
+				} else {
+					this[k] = waitData[k]
+				}
+			}
+			wait2Update[viewname][viewtag] = null
+		}
+		for(var commonk in _viewDatas.common) {
+			updateCommonDataHelper(this, commonk, _viewDatas.common[commonk])
+		}
+	}
+	var oldBeforeDestroy = config.beforeDestroy
+	config.beforeDestroy = function() {
+		oldBeforeDestroy && oldBeforeDestroy.bind(this)()
+		for(var n=0,nn=_vms.length;n<nn;n++) {
+			if(_vms[n] === this) {
+				_vms.splice(n, 1)
+				break
+			}
+		}
+		if(this.cache) {
+			var viewtag = this._props.viewtag || 'default'
+			_viewDatas[this._viewname][viewtag] = util.$deepCopy(this._data)
+		} else {
+			_viewDatas[this._viewname][viewtag] = {}
+		}
+	}
 	_viewDatas[uuid] = {}
 	var callbacks = ['beforeCreate', 'created', 'beforeMount', 'mounted', 'beforeUpdate', 'updated', 'beforeDestroy', 'destroyed']
 	for(var k in config) {
 		if(config.hasOwnProperty(k) && !isUndefinedOrNull(config[k])) {
 			this[k] = config[k];
 		}
-	}
-	for(var i = 0; i < callbacks.length; i++) {
-		if(!this[callbacks[i]]) {
-			this[callbacks[i]] = function() {}
-		}
-		var _this = this;
-		(function(i) {
-			_this[callbacks[i]] = _this[callbacks[i]]._before_(function() {
-				if(i === 0) { // beforeCreate
-					_vms.push(this)
-				}
-				if(i === 2) { // beforeMount
-					this.configviewname = viewname
-					this._viewname = uuid
-					this.cache = cache
-					this.$set(this.common, "randNum", util.$getUuid())
-					var viewtag = this._props.viewtag || 'default'
-					if(this.cache) { // 如果需要缓存的话就要把该baseview的对象data加入字段
-						for(var k in _viewDatas[this._viewname][viewtag]) {
-							if(this[k] === undefined) this.$set(this.$data, k, null)
-						}
-					}
-				}
-// 				if(i === 3) { // mounted
-// 					this.$el.classList.add('_js_viewdata_' + uuid + '-' + viewtag) //给每个继承自该对象的页面根节点添加样式
-// 				}
-				_this['_' + callbacks[i]] && _this['_' + callbacks[i]]()
-			})._after_(function() {
-				if(i === 3) { // mounted
-					var viewtag = this._props.viewtag || 'default'
-					if(this.cache) { // 有指定该baseview是缓存的话就要在渲染完之后加入缓存内容
-						var viewDatas = _viewDatas[this._viewname][viewtag]
-						for(var k in viewDatas) {
-							if(this[k] === viewDatas[k]) continue // 没变化，不更新
-							if(Object.prototype.toString.call(viewDatas[k]) === '[object Array]') {
-								for(var j=0;j<viewDatas[k].length;j++) {
-									this.$set(this[k], j, util.$deepCopy(viewDatas[k][j]))
-								}
-							} else if(Object.prototype.toString.call(viewDatas[k]) === '[object object]') {
-								for(var _key in viewDatas[k]) {
-									this.$set(this[k], _key, viewDatas[k][_key])
-								}
-							} else {
-								this[k] = viewDatas[k]
-							}
-						}
-						_viewDatas[this._viewname][viewtag] = null
-					}
-					if(wait2Update[viewname] && wait2Update[viewname][viewtag]) {
-						var waitData = wait2Update[viewname][viewtag]
-						for(var k in waitData) {
-							if(this[k] === waitData[k]) continue // 没变化，不更新
-							if(Object.prototype.toString.call(waitData[k]) === '[object Array]') {
-								for(var j=0;j<waitData[k].length;j++) {
-									this.$set(this[k], j, util.$deepCopy(waitData[k][j]))
-								}
-							} else if(Object.prototype.toString.call(waitData[k]) === '[object object]') {
-								for(var _key in waitData[k]) {
-									this.$set(this[k], _key, waitData[k][_key])
-								}
-							} else {
-								this[k] = waitData[k]
-							}
-						}
-						wait2Update[viewname][viewtag] = null
-					}
-					for(var commonk in _viewDatas.common) {
-						updateCommonDataHelper(this, commonk, _viewDatas.common[commonk])
-					}
-				}
-				if(i === 6) { // beforeDestroy
-					for(var n=0,nn=_vms.length;n<nn;n++) {
-						if(_vms[n] === this) {
-							_vms.splice(n, 1)
-							break
-						}
-					}
-					if(this.cache) {
-						var viewtag = this._props.viewtag || 'default'
-						_viewDatas[this._viewname][viewtag] = util.$deepCopy(this._data)
-					} else {
-						_viewDatas[this._viewname][viewtag] = {}
-					}
-				}
-				_this[callbacks[i] + '_'] && _this[callbacks[i] + '_']()
-			})
-		})(i)
 	}
 }
 window.VueData = VueData
