c72c4a0efdd3c8cb6f9751b03ce76711b36d544b
avengang <544160071@qq.com>
2018-09-02 13:59:21 +0800

    修复缓存bug

--- index.js
+++ index.js
@@ -2,186 +2,194 @@
 import util from './util.js'
 var name_tags = {}
 var _viewDatas = {
-	common: {}
+  common: {}
 }
 var _vms = []
 var wait2Update = {}
 function vuedataDo() {
-	var viewname, viewtag, key, value, method, isMethod = false
-	if(arguments.length === 4) { // viewname, viewtag, key, value
-		viewname = arguments[0]
-		viewtag = arguments[1]
-		key = arguments[2]
-		value = arguments[3]
-	} else if(arguments.length === 3) { // viewname, viewtag, method
-		viewname = arguments[0]
-		viewtag = arguments[1]
-		method = arguments[2]
-		isMethod = true
-	} else if(arguments.length === 2) { // 更新全局属性
-		updateCommonData(arguments[0], arguments[1])
-		return
-	} else {
-		console.log('传入参数：', arguments)
-		throw new Error('$vuedataDo参数个数不匹配，参数个数必须为3（viewname, viewtag, method）或者4个(viewname, viewtag, key, value)：')
-	}
-	viewtag = viewtag || 'default'
-	for(var n=0,nn=_vms.length;n<nn;n++) {
-		var vm = _vms[n]
-		var _viewtag = vm._props.viewtag || 'default'
-		if(vm.configviewname === viewname) {
-			if(+viewtag !== -1) {
-				if(_viewtag !== viewtag) {
-					continue
-				}
-				if(isMethod) {
-					vm[method] && vm[method]()
-				} else {
-					util.$setSingle(key, value, vm)
-				}
-				return
-			} else {
-				if(isMethod) {
-					vm[method] && vm[method]()
-				} else {
-					util.$setSingle(key, value, vm)
-				}
-			}
-		}
-	}
-	if(viewtag !== -1 && !isMethod) {
-		if(!wait2Update[viewname])
-			wait2Update[viewname] = {}
-		if(!wait2Update[viewname][viewtag])
-			wait2Update[viewname][viewtag] = {}
-		wait2Update[viewname][viewtag][key] = util.$deepCopy(value)
-	}
+  var viewname, viewtag, key, value, method, isMethod = false
+  if(arguments.length === 4) { // viewname, viewtag, key, value
+    viewname = arguments[0]
+    viewtag = arguments[1]
+    key = arguments[2]
+    value = arguments[3]
+  } else if(arguments.length === 3) { // viewname, viewtag, method
+    viewname = arguments[0]
+    viewtag = arguments[1]
+    method = arguments[2]
+    isMethod = true
+  } else if(arguments.length === 2) { // 更新全局属性
+    updateCommonData(arguments[0], arguments[1])
+    return
+  } else {
+    console.log('传入参数：', arguments)
+    throw new Error('$vuedataDo参数个数不匹配，参数个数必须为3（viewname, viewtag, method）或者4个(viewname, viewtag, key, value)：')
+  }
+  viewtag = viewtag || 'default'
+  for(var n=0,nn=_vms.length;n<nn;n++) {
+    var vm = _vms[n]
+    var _viewtag = vm._props.viewtag || 'default'
+    if(vm.configviewname === viewname) {
+      if(+viewtag !== -1) {
+        if(_viewtag !== viewtag) {
+          continue
+        }
+        if(isMethod) {
+          vm[method] && vm[method]()
+        } else {
+          util.$setSingle(key, value, vm)
+        }
+        return
+      } else {
+        if(isMethod) {
+          vm[method] && vm[method]()
+        } else {
+          util.$setSingle(key, value, vm)
+        }
+      }
+    }
+  }
+  if(viewtag !== -1 && !isMethod) {
+    if(!wait2Update[viewname])
+      wait2Update[viewname] = {}
+    if(!wait2Update[viewname][viewtag])
+      wait2Update[viewname][viewtag] = {}
+    wait2Update[viewname][viewtag][key] = util.$deepCopy(value)
+  }
 }
 function updateCommonDataHelper(vm, key, value) {
-	if(!vm.common) {
-		return //其他未从VueData继承的页面
-	}
-	if(Object.prototype.toString.call(value) === '[object Array]') {
-		if(vm.common[key] === value) return
-		for(var j=0;j<value.length;j++) {
-			vm.$set(vm.common[key], j, util.$deepCopy(value[j]))
-		}
-	} else if(Object.prototype.toString.call(value) === '[object object]') {
-		if(vm.common[key] === value) return
-		for(var _key in value) {
-			vm.$set(vm.common[key], _key, value[_key])
-		}
-	} else {
-		var obj = {}
-		obj[key] = value
-		vm.$set(vm.common, key, util.$deepCopy(value))
-	}
+  if(!vm.common) {
+    return //其他未从VueData继承的页面
+  }
+  if(Object.prototype.toString.call(value) === '[object Array]') {
+    if(vm.common[key] === value) return
+    for(var j=0;j<value.length;j++) {
+      vm.$set(vm.common[key], j, util.$deepCopy(value[j]))
+    }
+  } else if(Object.prototype.toString.call(value) === '[object object]') {
+    if(vm.common[key] === value) return
+    for(var _key in value) {
+      vm.$set(vm.common[key], _key, value[_key])
+    }
+  } else {
+    var obj = {}
+    obj[key] = value
+    vm.$set(vm.common, key, util.$deepCopy(value))
+  }
 }
 function updateCommonData(key, value) {
-	_viewDatas.common[key] = value
-	for(var i=0,ii=_vms.length;i<ii;i++) {
-		var vm = _vms[i]
-		updateCommonDataHelper(vm, key, value)
-	}
+  _viewDatas.common[key] = value
+  for(var i=0,ii=_vms.length;i<ii;i++) {
+    var vm = _vms[i]
+    updateCommonDataHelper(vm, key, value)
+  }
 }
 var VueData = function(config) {
-	var cache = config.cache
-	var uuid = util.$getUuid()
-	var viewname = config.viewname || uuid
-	if(name_tags[viewname]) throw new Error('viewname不能重复, 已经存在viewname = ' + config.viewname + ' 的对象')
-	name_tags[viewname] = {}
-	var dataReturn = null
-	if(config.data && Object.prototype.toString.call(config.data) === "[object Function]") {
-		try{
-			dataReturn = config.data()
-		}catch(e){
-			console.warn('VueData构造方法中data方法返回的对象中不允许使用this的属性，因为此时对象还未构造中，建议在created方法中给变量赋值。')
-			console.error(e)
-		}
-	}
-	if(!config.data || !dataReturn) {
-		config.data = function() {
-			return util.$deepCopy({
-				common: {}
-			})
-		}
-	} else {
-		var d = dataReturn
-		d.common = {}
-		config.data = function() {
-			return util.$deepCopy(d)
-		}
-	}
-	if(!config.props) {
-		config.props = []
-		config.props.push('viewtag')
-	} else {
-		if(Object.prototype.toString.call(config.props) === '[object Array]') { // 数组
-			config.props.push('viewtag')
-		} else { // 对象
-			config.props.viewtag = String
-		}
-	}
-	var oldBeforeMount = config.beforeCreate
-	config.beforeCreate = function() {
-		_vms.push(this)
-		oldBeforeMount && oldBeforeMount.bind(this)()
-	}
-	var oldBeforeMount = config.beforeMount
-	config.beforeMount = function() {
-		this.configviewname = viewname
-		this._viewname = uuid
-		this.cache = cache
-		var viewtag = this._props.viewtag || 'default'
-		if(name_tags[viewname][viewtag]) throw new Error('同一个viewname（' + viewname + '）下不能重复定义同一个viewtag:' + viewtag)
-		name_tags[viewname][viewtag] = true
-		if(this.cache) { // 如果需要缓存的话就要把该对象data加入字段
-			for(var k in _viewDatas[this._viewname][viewtag]) {
-				if(this[k] === undefined) this.$set(this.$data, k, null)
-			}
-		}
-		oldBeforeMount && oldBeforeMount.bind(this)()
-	}
-	var oldMounted = config.mounted
-	config.mounted = function() {
-		oldMounted && oldMounted.bind(this)()
-		var viewtag = this._props.viewtag || 'default'
-		if(this.cache) { // 有指定该对象需要缓存的话就要在渲染完之后加入缓存内容
-			var viewDatas = _viewDatas[this._viewname][viewtag]
-			util.$set(viewDatas, this)
-			_viewDatas[this._viewname][viewtag] = null
-		}
-		if(wait2Update[viewname] && wait2Update[viewname][viewtag]) {
-			var waitData = wait2Update[viewname][viewtag]
-			util.$set(waitData, this)
-			wait2Update[viewname][viewtag] = null
-		}
-		for(var commonk in _viewDatas.common) {
-			updateCommonDataHelper(this, commonk, _viewDatas.common[commonk])
-		}
-	}
-	var oldBeforeDestroy = config.beforeDestroy
-	config.beforeDestroy = function() {
-		oldBeforeDestroy && oldBeforeDestroy.bind(this)()
-		for(var n=0,nn=_vms.length;n<nn;n++) {
-			if(_vms[n] === this) {
-				_vms.splice(n, 1)
-				break
-			}
-		}
-		if(this.cache) {
-			var viewtag = this._props.viewtag || 'default'
-			_viewDatas[this._viewname][viewtag] = util.$deepCopy(this._data)
-		} else {
-			_viewDatas[this._viewname][viewtag] = {}
-		}
-	}
-	_viewDatas[uuid] = {}
-	return config
+  var cache = config.cache
+  var uuid = util.$getUuid()
+  var viewname = config.viewname || uuid
+  if(name_tags[viewname]) throw new Error('viewname不能重复, 已经存在viewname = ' + config.viewname + ' 的对象')
+  name_tags[viewname] = {}
+  var dataReturn = null
+  if(config.data && Object.prototype.toString.call(config.data) === "[object Function]") {
+    try{
+      dataReturn = config.data()
+    }catch(e){
+      console.warn('VueData构造方法中data方法返回的对象中不允许使用this的属性，因为此时对象还未构造中，建议在created方法中给变量赋值。')
+      console.error(e)
+    }
+  }
+  if(!config.data || !dataReturn) {
+    config.data = function() {
+      return util.$deepCopy({
+        common: {}
+      })
+    }
+  } else {
+    var d = dataReturn
+    d.common = {}
+    config.data = function() {
+      return util.$deepCopy(d)
+    }
+  }
+  if(!config.props) {
+    config.props = []
+    config.props.push('viewtag')
+  } else {
+    if(Object.prototype.toString.call(config.props) === '[object Array]') { // 数组
+      config.props.push('viewtag')
+    } else { // 对象
+      config.props.viewtag = String
+    }
+  }
+  var oldBeforeCreate = config.beforeCreate
+  config.beforeCreate = function() {
+    _vms.push(this)
+    oldBeforeCreate && oldBeforeCreate.bind(this)()
+  }
+  var oldCreated = config.created
+  config.created = function() {
+    this.$$cache = cache || util.$getCache(this)
+    var viewtag = this._props.viewtag || 'default'
+    if(this.$$cache || (this.$$cache && !name_tags[viewname][viewtag])) {
+      oldBeforeCreate && oldBeforeCreate.bind(this)()
+      oldCreated && oldCreated.bind(this)()
+    }
+  }
+  var oldBeforeMount = config.beforeMount
+  config.beforeMount = function() {
+    this.configviewname = viewname
+    var viewtag = this._props.viewtag || 'default'
+    if(!this.$$cache && name_tags[viewname][viewtag]) throw new Error('同一个viewname（' + viewname + '）下不能重复定义同一个viewtag:' + viewtag)
+    name_tags[viewname][viewtag] = true
+    if(this.$$cache) { // 如果需要缓存的话就要把该对象data加入字段
+      for(var k in _viewDatas[uuid][viewtag]) {
+        if(this[k] === undefined) this.$set(this.$data, k, null)
+      }
+    }
+    if(!this.$$cache || (this.$$cache && !name_tags[viewname][viewtag])) oldBeforeMount && oldBeforeMount.bind(this)()
+  }
+  var oldMounted = config.mounted
+  config.mounted = function() {
+    var viewtag = this._props.viewtag || 'default'
+    if(!this.$$cache || (this.$$cache && !name_tags[viewname][viewtag])) oldMounted && oldMounted.bind(this)()
+    if(this.$$cache) { // 有指定该对象需要缓存的话就要在渲染完之后加入缓存内容
+      var viewDatas = _viewDatas[uuid][viewtag]
+      util.$set(viewDatas, this)
+      _viewDatas[uuid][viewtag] = null
+    }
+    if(wait2Update[viewname] && wait2Update[viewname][viewtag]) {
+      var waitData = wait2Update[viewname][viewtag]
+      util.$set(waitData, this)
+      wait2Update[viewname][viewtag] = null
+    }
+    for(var commonk in _viewDatas.common) {
+      updateCommonDataHelper(this, commonk, _viewDatas.common[commonk])
+    }
+  }
+  var oldBeforeDestroy = config.beforeDestroy
+  config.beforeDestroy = function() {
+    var viewtag = this._props.viewtag || 'default'
+    oldBeforeDestroy && oldBeforeDestroy.bind(this)()
+    for(var n=0,nn=_vms.length;n<nn;n++) {
+      if(_vms[n] === this) {
+        _vms.splice(n, 1)
+        break
+      }
+    }
+    if(this.$$cache) {
+      _viewDatas[uuid][viewtag] = util.$deepCopy(this._data)
+    } else {
+      name_tags[viewname][viewtag] = undefined
+      _viewDatas[uuid][viewtag] = null
+    }
+  }
+  _viewDatas[uuid] = {}
+  return config
 }
 VueData.$vuedataDo = vuedataDo
 window.VueData = VueData
 function install(Vue, options) {
-	Vue.prototype.$vuedataDo = vuedataDo
+  Vue.prototype.$vuedataDo = vuedataDo
 }
 export default install
--- util.js
+++ util.js
@@ -32,54 +32,62 @@ var $deepCopy = function (obj, cache = []) {
   return copy
 }
 var $getElLink = function(el) {
-	var result = ''
-	while(el) {
-		result += el.nodeName + el.id + el.className
-		el = el.parent
-	}
-	return result
+  var result = ''
+  while(el) {
+    result += el.nodeName + el.id + el.className
+    el = el.parent
+  }
+  return result
 }
 
 var $getUuid = function() {
-	var s = [];
-	var hexDigits = "0123456789abcdef";
-	for (var i = 0; i < 36; i++) {
-			s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
-	}
-	s[14] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
-	s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1); // bits 6-7 of the clock_seq_hi_and_reserved to 01
-	s[8] = s[13] = s[18] = s[23] = "_";
-	var uuid = s.join("");
-	return uuid;
+  var s = [];
+  var hexDigits = "0123456789abcdef";
+  for (var i = 0; i < 36; i++) {
+      s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+  }
+  s[14] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
+  s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1); // bits 6-7 of the clock_seq_hi_and_reserved to 01
+  s[8] = s[13] = s[18] = s[23] = "_";
+  var uuid = s.join("");
+  return uuid;
 }
 var $setSingle = function(k, value, dist) {
-	if(dist[k] === value) return // 没变化，不更新
-	if(Object.prototype.toString.call(value) === '[object Array]') {
-		for(var j=0;j<value.length;j++) {
-			dist.$set(dist[k], j, $deepCopy(value[j]))
-		}
-	} else if(Object.prototype.toString.call(value) === '[object object]') {
-		for(var _key in value) {
-			dist.$set(dist[k], _key, value[_key])
-		}
-	} else {
-		dist[k] = value
-	}
+  if(dist[k] === value) return // 没变化，不更新
+  if(Object.prototype.toString.call(value) === '[object Array]') {
+    for(var j=0;j<value.length;j++) {
+      dist.$set(dist[k], j, $deepCopy(value[j]))
+    }
+  } else if(Object.prototype.toString.call(value) === '[object object]') {
+    for(var _key in value) {
+      dist.$set(dist[k], _key, value[_key])
+    }
+  } else {
+    dist[k] = value
+  }
 }
 var $set = function(src, dist) {
-	for(var k in src) {
-		$setSingle(k, src[k], dist)
-	}
+  for(var k in src) {
+    $setSingle(k, src[k], dist)
+  }
 }
 var $isUndefinedOrNull = function(arg) {
-	if(arg === undefined || arg === null) return true
-	return false
+  if(arg === undefined || arg === null) return true
+  return false
+}
+var $getCache = function(vm) {
+  while(vm) {
+    if(!!vm.$$cache) return true
+    vm = vm.$parent
+  }
+  return false
 }
 export default {
-	$deepCopy,
-	$getElLink,
-	$getUuid,
-	$set,
-	$setSingle,
-	$isUndefinedOrNull
+  $deepCopy,
+  $getElLink,
+  $getUuid,
+  $set,
+  $setSingle,
+  $isUndefinedOrNull,
+  $getCache
 }
